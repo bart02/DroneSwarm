a6b5dc0a83a78c6ede632ea66622e18261530883
Artem Batalov <bat.artem2002@ya.ru>
2018-07-03 23:40:19 +0300

    For Python 2.7

--- UserApps/aruco-generator/aruco-generator.py
+++ UserApps/aruco-generator/aruco-generator.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 x = int(input())
 y = int(input())
 first = int(input()) - 1
@@ -6,17 +8,17 @@ My = []
 for e in range(x):
     for i in range(y):
         My.append(first + 1)
-        first+=1
+        first += 1
     Mx.append(My)
     My = []
 
 print("[", end='')
 for i in range(y):
     for e in range(x):
-        if (i == y-1 and e == x-1):
+        if i == y - 1 and e == x - 1:
             print(Mx[e][i], end='')
         else:
             print(Mx[e][i], end=', ')
-    if (i != y-1):
+    if i != y - 1:
         print()
 print("]")
