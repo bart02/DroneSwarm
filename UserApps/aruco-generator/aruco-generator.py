0b4f46e2d6c8c588d520177786bf687a235d04fd
Artem Batalov <bat.artem2002@ya.ru>
2018-07-04 10:31:28 +0300

    New aruco generator

--- UserApps/aruco-generator/aruco-generator.py
+++ UserApps/aruco-generator/aruco-generator.py
@@ -1,24 +1,29 @@
 from __future__ import print_function
 
-x = int(input())
-y = int(input())
-first = int(input()) - 1
+x = int(input("X: "))
+y = int(input("Y: "))
+l = int(input("Length of column: "))
+first = int(input("First: ")) - 1
 Mx = []
 My = []
-for e in range(x):
-    for i in range(y):
-        My.append(first + 1)
-        first += 1
-    Mx.append(My)
+O = []
+for ll in range(x/l):
+    for e in range(y):
+        for i in range(l):
+            Mx.append(first + 1)
+            first += 1
+        My.append(Mx)
+        Mx = []
+    O.append(My)
     My = []
 
-print("[", end='')
-for i in range(y):
-    for e in range(x):
-        if i == y - 1 and e == x - 1:
-            print(Mx[e][i], end='')
-        else:
-            print(Mx[e][i], end=', ')
-    if i != y - 1:
-        print()
+print("[")
+for e in range(len(O[0])):
+    for j in range(len(O)):
+        for q in O[j][e]:
+            if e == len(O[0]) - 1 and j == len(O) - 1 and q == O[j][e][len(O[j][e])-1]:
+                print(q, end='')
+            else:
+                print(q, end=', ')
+    print()
 print("]")
