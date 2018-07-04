b8ceedbb9db7c5f7860d5d0bb323fe191a42399d
Artem30801 <artem30801@gmail.com>
2018-07-04 23:29:36 +0300

    Fully implemented all main flight functions

--- Drone/FlightLib/FlightLib.py
+++ Drone/FlightLib/FlightLib.py
@@ -15,17 +15,33 @@ navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 set_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
 
 # variables
-telem = get_telemetry(frame_id=frame_id)  # initing telemetry
-yaw_current = telem.yaw
-z_current = telem.z
+yaw_current = 0
+x_current = 0
+y_current = 0
+z_current = 0
 
 # functions
+
 
 def get_distance(x1, y1, z1, x2, y2, z2):
 	return math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
 
 
-def reach(x, y, z, yaw=yaw_current, speed=1, tolerance=0.15, frame_id='aruco_map', freq=10):
+def capture_position():
+    telem = get_telemetry(frame_id='aruco_map')
+
+    global yaw_current
+    global x_current
+    global y_current
+    global z_current
+
+    yaw_current = telem.yaw
+    x_current = telem.x
+    y_current = telem.y
+    z_current = telem.z
+
+
+def reach(x, y, z, yaw=yaw_current, speed=1, tolerance=0.15, frame_id='aruco_map', freq=5):
 	telem = get_telemetry(frame_id=frame_id)
     navigate(frame_id=frame_id, x=x, y=y, z=z, yaw=yaw, speed=speed)
     print ("Reaching point:", "x=", x, ", y=", y, ", z=", z, "yaw=", yaw)
@@ -35,14 +51,28 @@ def reach(x, y, z, yaw=yaw_current, speed=1, tolerance=0.15, frame_id='aruco_map
 		telem = get_telemetry(frame_id=frame_id)
         print("Reaching point, telem:", "x=", telem.x, ", y=", telem.y, ", z=", telem.z, "yaw=", telem.yaw)
 		rospy.sleep(1/freq)
+    print("Point reached!")
 
-def takeoff(z, speed=1, frame_id='fcu_horiz'): #be reworked soon
+def attitude(z, yaw=yaw_current, speed=1, tolerance=0.15, frame_id='aruco_map'):
+    print("Reaching attitude")
+    capture_position()
+    reach(x=x_current, y=y_current, z=z, yaw=yaw, speed=speed, tolerance=tolerance, frame_id=frame_id)
+    print("Attitude reached!")
+
+def takeoff(z=1, speed=1, frame_id='fcu_horiz'):
     print("Taking off!")
     navigate(frame_id=frame_id, x=0, y=0, z=z, speed=speed, update_frame=False, auto_arm=True)
     rospy.sleep(5)
-    print("Took off!")
+    print("In air!")
 
-def land(): #not completed
+    attitude(z, tolerance=0.25)
+    print("Takeoff completed!")
+
+def land(z=0.5):
     print("Landing!")
+    attitude(z, tolerance=0.2)
+    print("Ready to land")
+
     set_mode(base_mode=0, custom_mode='AUTO.LAND')
-    print("Landed!")
+    print("Land completed!")
+
