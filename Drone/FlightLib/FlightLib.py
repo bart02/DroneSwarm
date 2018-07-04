c14167da9625168153cdc33274ee4775020e68ce
Artem30801 <artem30801@gmail.com>
2018-07-05 00:13:30 +0300

    Fixed math

--- Drone/FlightLib/FlightLib.py
+++ Drone/FlightLib/FlightLib.py
@@ -21,11 +21,10 @@ x_current = 0
 y_current = 0
 z_current = 0
 
+
 # functions
-
-
 def get_distance(x1, y1, z1, x2, y2, z2):
-	return math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
 
 
 def capture_position():
@@ -45,20 +44,22 @@ def capture_position():
 def reach(x, y, z, yaw=yaw_current, speed=1, tolerance=0.15, frame_id='aruco_map', freq=5):
     telem = get_telemetry(frame_id=frame_id)
     navigate(frame_id=frame_id, x=x, y=y, z=z, yaw=yaw, speed=speed)
-    print ("Reaching point:", "x=", x, ", y=", y, ", z=", z, "yaw=", yaw)
+    print("Reaching point:", "x=", x, ", y=", y, ", z=", z, "yaw=", yaw)
 
-    # waiting for complition
-	while get_distance(x, y, z, telem.x, telem.y, telem.z) > tolerance:
+    # waiting for completion
+    while get_distance(x, y, z, telem.x, telem.y, telem.z) > tolerance:
         telem = get_telemetry(frame_id=frame_id)
         print("Reaching point, telem:", "x=", telem.x, ", y=", telem.y, ", z=", telem.z, "yaw=", telem.yaw)
-        rospy.sleep(1/freq)
+        rospy.sleep(1 / freq)
     print("Point reached!")
+
 
 def attitude(z, yaw=yaw_current, speed=1, tolerance=0.15, frame_id='aruco_map'):
     print("Reaching attitude")
     capture_position()
     reach(x=x_current, y=y_current, z=z, yaw=yaw, speed=speed, tolerance=tolerance, frame_id=frame_id)
     print("Attitude reached!")
+
 
 def takeoff(z=1, speed=1, frame_id='fcu_horiz'):
     print("Taking off!")
@@ -69,6 +70,7 @@ def takeoff(z=1, speed=1, frame_id='fcu_horiz'):
     attitude(z, tolerance=0.25)
     print("Takeoff completed!")
 
+
 def land(z=0.5):
     print("Landing!")
     attitude(z, tolerance=0.2)
@@ -76,4 +78,3 @@ def land(z=0.5):
 
     set_mode(base_mode=0, custom_mode='AUTO.LAND')
     print("Land completed!")
-
