587d2ca9f27a52cbbfdcf12e2dc4a06472f23b56
Artem30801 <artem30801@gmail.com>
2018-07-07 16:45:23 +0300

    Various fixes

--- Drone/ledTest.py
+++ Drone/ledTest.py
@@ -1,15 +1,16 @@
 from FlightLib import LedLib as led
 import time
 
+time.sleep(0.1)
 print("Starting test...")
 led.fill(255, 0, 0)
-time.sleep(1)
+time.sleep(10)
 led.wipe_to(0, 255, 0)
-time.sleep(1)
+time.sleep(10)
 led.rainbow()
-time.sleep(1)
+time.sleep(10)
 led.chase(0, 0, 255)
-time.sleep(1)
+time.sleep(10)
 led.blink(255, 255, 255)
-time.sleep(1)
+time.sleep(10)
 led.off()
