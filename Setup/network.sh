3d948e3fde9c45a7633cc011e41acbd4a833851a
Artem Batalov <bat.artem2002@ya.ru>
2018-07-08 15:02:39 +0300

    Fix network.sh

--- Setup/network.sh
+++ Setup/network.sh
@@ -17,7 +17,7 @@ sudo systemctl disable dnsmasq
 sudo sed -i 's/interface wlan0//' /etc/dhcpcd.conf
 sudo sed -i 's/static ip_address=192.168.11.1\/24//' /etc/dhcpcd.conf
 
-cat << EOF | tee wpa_supplicant.conf
+cat << EOF | sudo tee /etc/wpa_supplicant/wpa_supplicant.conf
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 country=GB
