f6eefe9f8366341ed1db4675c4717f67ab064a8f
Artem30801 <artem30801@gmail.com>
2018-07-09 13:59:02 +0300

    Added closing

--- Drone/Client.py
+++ Drone/Client.py
@@ -3,16 +3,19 @@ import socket
 sock=socket.socket()
 sock.connect(('192.168.1.5', 35001))
 
-
-while True:
-    data = sock.recv(1024)
-    if data == b'fill':
-        led.run(255, 0, 255)
-    if data == b'run':
-        led.fill(255, 0, 255)
-    if data == b'blink':
-        led.blink(255, 0, 255)
-    if data == b'off':
-        led.off()
-sock.close()
+try:
+    while True:
+        data = sock.recv(1024)
+        if data == b'fill':
+            led.run(255, 0, 255)
+        if data == b'run':
+            led.fill(255, 0, 0)
+        if data == b'blink':
+            led.blink(255, 0, 255)
+        if data == b'off':
+            led.off()
+except KeyboardInterrupt:
+    print ("Shutting down listener")
+    sock.close()
+    led.off()
 
