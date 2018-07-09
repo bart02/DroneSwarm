e8a8d9bf0a11fe239554d67a0bd581d4e233066d
Alexandr <37707797+Neptune-1@users.noreply.github.com>
2018-07-09 14:34:42 +0300

    Update Client.py

--- Drone/Client.py
+++ Drone/Client.py
@@ -6,10 +6,13 @@ sock.connect(('192.168.1.5', 35001))
 
 while True:
     data = sock.recv(1024)
+ 
     try:
-        eval(str(data))
+        if len(data) == data.index(b'$$')+1:
+            eval(str(data[:-2]))
+        else:
+            eval(str(data[:data.index('$$')]))
+            eval(str(data[data.index('$$') + 2:len(data)]))
     except():
         print('er')
 sock.close()
-
-
