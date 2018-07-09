03542c2e8001223f6f4f57689c6a4984e7ccdad5
Alexandr <37707797+Neptune-1@users.noreply.github.com>
2018-07-09 13:44:14 +0300

    Update Client.py

--- Drone/Client.py
+++ Drone/Client.py
@@ -7,11 +7,11 @@ sock.connect(('192.168.1.5', 35001))
 while True:
     data = sock.recv(1024)
     if data == b'fill':
-        led.fill(255, 255, 255)
+        led.run(255, 0, 255)
     if data == b'run':
-        led.fill(255, 255, 255)
+        led.fill(255, 0, 255)
     if data == b'blink':
-        led.blink(255, 255, 255)
+        led.blink(255, 0, 255)
     if data == b'off':
         led.off()
 sock.close()
