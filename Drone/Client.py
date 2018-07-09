1ab146f133ad006068b426fbd102e9bcf5f843b5
Alexandr <37707797+Neptune-1@users.noreply.github.com>
2018-07-09 14:08:59 +0300

    Update Client.py

--- Drone/Client.py
+++ Drone/Client.py
@@ -3,19 +3,13 @@ import socket
 sock=socket.socket()
 sock.connect(('192.168.1.5', 35001))
 
-try:
-    while True:
-        data = sock.recv(1024)
-        if data == b'fill':
-            led.run(255, 0, 255)
-        if data == b'run':
-            led.fill(255, 0, 0)
-        if data == b'blink':
-            led.blink(255, 0, 255)
-        if data == b'off':
-            led.off()
-except KeyboardInterrupt:
-    print ("Shutting down listener")
-    sock.close()
-    led.off()
+
+while True:
+    data = sock.recv(1024)
+    try:
+        eval(str(data))
+    except():
+        print('er')
+sock.close()
+
 
