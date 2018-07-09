9ddd20af675aae7381d7f1fdee06fb7efef18156
Artem30801 <artem30801@gmail.com>
2018-07-09 14:52:29 +0300

    Fixed shutting down led on exit

--- Drone/Client.py
+++ Drone/Client.py
@@ -1,17 +1,20 @@
 from FlightLib import LedLib as led
 import socket
-sock=socket.socket()
+sock = socket.socket()
 sock.connect(('192.168.1.5', 35001))
 
+try:
+    while True:
+        data = str(sock.recv(1024))
 
-while True:
-    data = str(sock.recv(1024))
-   
-    try:
-        if '$$' in data:
-            eval(str(data[ : data.index(b'$$')]))
+        try:
+            if '$$' in data:
+                eval(str(data[ : data.index(b'$$')]))
 
-    except():
-        print('er')
-sock.close()
+        except():
+            print('er')
+except KeyboardInterrupt:
+    print("Shutting down")
+    led.off()
+    sock.close()
 
