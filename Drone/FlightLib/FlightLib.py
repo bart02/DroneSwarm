e0ab6c6ef75d9dec038aea63062eca38346aea98
Artem30801 <artem30801@gmail.com>
2018-07-11 12:15:58 +0300

    HOTFIX

--- Drone/FlightLib/FlightLib.py
+++ Drone/FlightLib/FlightLib.py
@@ -145,12 +145,15 @@ def takeoff(z=1, speed=1, yaw=float('nan'), frame_id='fcu_horiz', tolerance=0.25
     print("In air!")
     rospy.sleep(1)
     time = 0
-    while abs(z - telem.z) > tolerance:
-        time += wait_ms
-        telem = get_telemetry(frame_id=frame_id)
-        rospy.sleep(wait_ms / 1000)
-        if timeout != 0 and (time >= timeout):
-            print("Not reached minimal takeoff attitude, trying to resolve...")
+    if timeout == 0:
+        while abs(z - telem.z) > tolerance:
+            time += wait_ms
+            telem = get_telemetry(frame_id=frame_id)
+            rospy.sleep(wait_ms / 1000)
+            if time >= timeout:
+                print("Not reached minimal takeoff attitude, trying to resolve...")
+    else:
+        rospy.sleep(5)
 
     print("Reaching takeoff attitude!")
     result = attitude(z, yaw, tolerance=0.25)
