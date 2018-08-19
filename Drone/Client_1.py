0c6a106eb2f160d7ba7c3747dabcdf9d324e7e4e
Alexandr <37707797+Neptune-1@users.noreply.github.com>
2018-08-19 22:22:10 +0300

    Update Client_1.py

--- Drone/Client_1.py
+++ Drone/Client_1.py
@@ -8,7 +8,6 @@ from time import sleep
 import socket
 import sys
 import time
-import xmltodict
 
 sq = []
 
@@ -23,308 +22,80 @@ while True:
         break
 
 def animation():
-        # xml parcer
-        time.sleep(4.5)
-
-        types = {
-
-            'x': float,
-
-            'y': float,
-
-            'z': float,
-
-            'yaw': float,
-
-            'yaw_rate': float,
-
-            'speed': float,
-
-            'tolerance': float,
-
-            'frame_id': str,
-
-            'mode': str,
-
-            'wait_ms': int,
-
-            'timeout': int,
-
-            'z_coefficient': float,
-
-            'timeout_arm': int,
-
-            'timeout_land': int,
-
-            'preland': bool,
-
-            'r': int,
-
-            'g': int,
-
-            'b': int,
-
-        }
-
-        def parse_xml(xml_file=None, xml_str=None):
-
-            if (xml_file is None and xml_str is None) or (xml_file is not None and xml_str is not None):
-                raise ValueError('You must use one parameter')
-
-            if xml_str is None:
-
-                with open(xml_file, 'r') as f:
-
-                    xml = f.read().strip()
-
-            else:
-
-                xml = xml_str
-
-            xmldict = xmltodict.parse(xml)
-
-            xmldict = dict(xmldict['DroneSwarm'])['time']
-
-            ready = {}
-
-            if type(xmldict) != list:
-                xmldict = [xmldict]
-
-            for t in xmldict:
-
-                time = float(t['@t'])
-
-                ready[time] = {}
-
-                try:
-
-                    if type(t['copter']) != list:
-                        t['copter'] = [t['copter']]
-
-                    for copter in t['copter']:
-
-                        copternum = int(copter['@n'])
-
-                        ready[time][copternum] = []
-
-                        copter.pop('@n')
-
-                        for action in copter:
-
-                            actiondict = {}
-
-                            try:
-
-                                for prm in dict(copter[action]):
-
-                                    val = dict(copter[action])[prm]
-
-                                    prm = str(prm.replace('@', ''))
-
-                                    try:
-
-                                        actiondict[prm] = types[prm](val)
-
-                                    except KeyError:
-
-                                        print("Types hasn't got " + prm + ', use str.')
-
-                                        actiondict[prm] = str(val)
-
-                                # print {action: actiondict}
-
-                                ready[time][copternum].append({str(action): actiondict})
-
-                            except ValueError:
-
-                                raise ValueError('You can use only "n" parameter in "copter" tag')
-
-                except KeyError:
-
-                    pass
-
-                try:
-
-                    swarm = t['swarm']
-
-                    copternum = 0
-
-                    ready[time][copternum] = []
-
-                    for action in swarm:
-
-                        actiondict = {}
-
-                        try:
-
-                            if swarm[action] is not None:
-
-                                for prm in dict(swarm[action]):
-
-                                    val = dict(swarm[action])[prm]
-
-                                    prm = str(prm.replace('@', ''))
-
-                                    try:
-
-                                        actiondict[prm] = types[prm](val)
-
-                                    except KeyError:
-
-                                        print("Types hasn't got " + prm + ', use str.')
-
-                                        actiondict[prm] = str(val)
-
-                            ready[time][copternum].append({str(action): actiondict})
-
-                        except TypeError:
-
-                            raise ValueError('You can use only one "swarm" tag')
-
-                except KeyError:
-
-                    pass
-
-            return ready
-
+        
         data = ''
-        xm = parse_xml('xml.xml')
-        print(xm)
-        n = 0
-        for i in xm:
 
-            for k in xm[i]:
-                # k =  copter number
-                s = str(xm.keys())[str(xm.keys()).index('[') + 1:-2]
-                p = s.split(', ')
-                try:
-                    timeout = str(((float(p[n + 1]) - float(p[n])) * 1000) - 2000)
-                except:
-                    print('end')
+        op = open('swarm.swarm', 'r')
+        read = op.read()
+        xm = read.split('*')
 
-                for l in xm[i][k]:
+        for i in range(1, len(xm)):
+            #print('i=', i)
+            #print('xm[i]=', xm[i])
+            for k in range(1, len(xm[i].split('/'))-1):
+                #print('k=', xm[i].split('/')[k])
+                timeout='1000'
 
-                    o = str(l.keys())
-                    f = o[o.index('[\'') + 2: -3]  # f =  function
+                for f in xm[i].split('/')[k].split(' '):
+                    #print('f=', f)
 
-                    # l[f] = parameters
-                    if k == 0:
-                        k = 'all'
 
-                    if f == 'circle':
-                        x = str(l[f]['x'])
-                        y = str(l[f]['y'])
-                        z = str(l[f]['z'])
-                        r = str(l[f]['r'])
+                    if f[0] == 'c':
 
-                        data = 'f.circle' + '(' + x + ',' + y + ',' + z + ',' + r + ',' + 'timeout =' + timeout + ')'
+                        data = 'led.off()'
 
-                        print(data, k)
+                        r = str(f[1:].split(',')[0])
+                        g = str(f[1:].split(',')[1])
+                        b = str(f[1:].split(',')[2])
+                        if float(r)+float(g)+float(b)==0:
+                            data = 'led.off()'
+                        else:
+                            data = 'led.fill' + '(' + g + ',' + r + ',' + b + ')'
+
+                        print(bytes(data, 'utf-8'), str(xm[i].split('/')[k][1]))
                         print('_______________________')
-                        self.sender(bytes(data, 'utf-8'), str(k))
-                    if f == 'music':
-                        file = str(l[f]['file'])
+                    elif f[0] == 'p':
+                        x = str(f[1:].split(',')[0])
+                        y = str(f[1:].split(',')[1])
+                        z = str(f[1:].split(',')[2])
 
-                        os.system(r"start" + file)
-                        time.sleep(3)
+                        speed = 'speed=0.6'
+                        data = 'f.reach' + '(' + x + ',' + y + ',' + z + ',' + speed + ',' + 'timeout=' + timeout + ')'
 
-
-                    elif f == 'led':
-                        print(n)
-                        r = float(l[f]['r'])
-                        g = float(l[f]['g'])
-                        b = float(l[f]['b'])
-
-                        led.fill(r, g, b)
-
-                        print(bytes(data, 'utf-8'), str(k))
+                        print(bytes(data, 'utf-8'), str(xm[i].split('/')[k][1]))
                         print('_______________________')
-                    elif f == 'reach':
-                        x = float(l[f]['x'])
-                        y = float(l[f]['y'])
-                        z = float(l[f]['z'])
-                        try:
-                            speed = float(l[f]['speed'])
-                            f.reach(x, y, z, speed=speed, timeout=timeout)
+                    elif f[:2] == 'tf':
+                        z = f[:2].split(',')[0]
 
-                        except:
-                            speed = 0.6
-                            f.reach(x, y, z, speed=speed, timeout=timeout)
 
-                        print(bytes(data, 'utf-8'), str(k))
-                        print('_______________________')
-                    elif f == 'takeoff':
-                        z = float(l[f]['z'])
-                        try:
-                            speed = float(l[f]['speed'])
-                            f.takeoff(z, timeout_arm=1000, speed=speed, timeout_fcu=(float(timeout) - 1000) // 2,
-                                      timeout=(float(timeout) - 1000) // 2)
+                        data = 'f.takeoff(' + z + ',' + 'timeout_arm=1000' + ',' + 'timeout_fcu=' + str(
+                            (float(timeout)) * 10) + ',' + 'timeout=' + str(
+                            (float(timeout)) * 10) + ')'
 
-                            print(bytes(data, 'utf-8'), str(k))
-                        except:
-                            f.takeoff(z, timeout_arm=1000, timeout_fcu=str((float(timeout) - 1000) // 2),
-                                      timeout=(float(timeout) - 1000) // 2)
-
-                            print(bytes(data, 'utf-8'), str(k))
+                        print(bytes(data, 'utf-8'), str(xm[i].split('/')[k][1]))
 
                         print('_______________________')
-                    elif f == 'land':
-                        f.land(timeout=float(timeout))
+                    elif f == 'ld':
+                        data = 'f.land(timeout=' + timeout + ')'
 
-                        print(bytes(data, 'utf-8'), str(k))
+                        print(bytes(data, 'utf-8'), str(xm[i].split('/')[k][1]))
                         print('_______________________')
-                    elif f == 'attitude':
-                        z = float(l[f]['z'])
-                        f.attitude(z, timeout=timeout)
-
-                        print(bytes(data, 'utf-8'), str(k))
-                        print('_______________________')
-                    elif f == 'stay':
+                        # elif f == 'attitude':
+                        #     z = str(l[f]['z'])
+                        #     data = 'f.attitude(' + z + ',' + 'timeout =' + timeout + ')'
+                        #
+                        #     self.sender(bytes(data, 'utf-8'), str(k[0]))
+                        #     print(bytes(data, 'utf-8'), str(k[0]))
+                        #     print('_______________________')
+                    elif f == 's':
                         x = str(l[f]['x'])
                         y = str(l[f]['y'])
 
-                        print(bytes(x + ' ' + y, 'utf-8'), str(k))
+                        print(bytes(x + ' ' + y, 'utf-8'), str(xm[i].split('/')[k][1]))
                         print('_______________________')
+            time.sleep(1)
 
-            s = str(xm.keys())[str(xm.keys()).index('[') + 1:-2]
-            p = s.split(', ')
-            print(p)
-
-            try:
-                time.sleep((float(timeout) + 1500) // 1000)
-
-            except:
-                print('end of list')
-            n += 1
-
-
-try:
-    while True:
-        data = str(sock.recv(1024))
-        print(data)
-        try:
-            if b'programm' in data:
-                print(1)
-                xm = open('xml.xml', 'w')
-                print(2)
-                data = data[data.index(b'programm') + 10:]
-                print(3)
-                xm.write(data)
-                print(4)
-
-                while True:
-                    print('5___________')
-                    
-                    data = str(sock.recv(1024))
-                    print(data)
-                    if b'$$' in data:
-                        xm.write(data[:data.index(b'$$')])
-                        print('stop_byte')
-                        xm.close()
-                        xm = open('xml.xml', 'r')
-                        
-                        print(xm.read())
-                        xm.close()
+        op.close()
                         
                         
                         
